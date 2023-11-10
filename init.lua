d79d21542688cc2d6a7f1770323c530a203264c6
ZerataX <mail@zera.tax>
2023-11-10 09:40:45 +0100

    plugins: mason: add languages

--- init.lua
+++ init.lua
@@ -568,19 +568,20 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
     },
   },
+  ruff_lsp = {},
+  -- nil_ls = {},
 }
 
 -- Setup neovim lua configuration

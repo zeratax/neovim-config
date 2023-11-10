2570ccbc83ba3448788b6d6cd2f7e064edcd1d95
ZerataX <mail@zera.tax>
2023-11-10 09:39:11 +0100

    keybindings: add select pasted region

--- init.lua
+++ init.lua
@@ -317,6 +317,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Select pasted text
+vim.keymap.set('n', 'gp', '`[v`]', { desc = 'Select [P]asted Region' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })

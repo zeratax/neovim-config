71fecda2f595b90a89ff04b69819e4a0b01f3dd8
ZerataX <mail@zera.tax>
2023-11-11 00:17:18 +0000

    plugins: nvim-gdb: init

--- init.lua
+++ init.lua
@@ -239,6 +239,7 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {'sakhnik/nvim-gdb' },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.

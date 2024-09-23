525042abfda1beb7bee0977cc98226d0e0c1ab7c
ZerataX <mail@zera.tax>
2024-09-23 23:24:58 +0200

    keybindings: add previous selection

--- init.lua
+++ init.lua
@@ -105,6 +105,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Select pasted text
+vim.keymap.set('n', 'vv', '`[v`]', { desc = 'Pasted Region' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
